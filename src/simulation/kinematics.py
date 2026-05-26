9fca27d480b68f88fed09c0b61cc21bd39f72cd7
Nishant2503 <amarng25@gmail.com>
2026-05-26 08:59:06 +0530

    kinematics2

--- src/simulation/kinematics.py
+++ src/simulation/kinematics.py
@@ -3,12 +3,11 @@ import pybullet_data
 import time
 import os
 
+
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.81)
-
 p.loadURDF("plane.urdf")
-
 urdf_path = "assets/urdf/so101.urdf"
 
 robot_id = p.loadURDF(
@@ -17,7 +16,7 @@ robot_id = p.loadURDF(
     useFixedBase=True
 )
 
-# Set camera view
+
 p.resetDebugVisualizerCamera(
     cameraDistance=0.8,
     cameraYaw=0,
@@ -25,35 +24,66 @@ p.resetDebugVisualizerCamera(
     cameraTargetPosition=[0, 0, 0.1]
 )
 
+
+target_position = [0.2, -0.2, 0.2]
+
 cube_id = p.loadURDF(
     "cube_small.urdf",
-    [0.2, -0.2, 0.2]
+    target_position
 )
 
-endEffector_link_index = 7
 
-end_pos = p.calculateInverseKinematics(
+end_effector_link_index = 7
+
+joint_angles = p.calculateInverseKinematics(
     bodyUniqueId=robot_id,
-    endEffectorLinkIndex=endEffector_link_index,
-    targetPosition=[0.2, -0.2, 0.2]
+    endEffectorLinkIndex=end_effector_link_index,
+    targetPosition=target_position
 )
 
-movable_joints = [1,2,3,4,5,7]
-k=0
+print("IK Joint Angles:")
+print(joint_angles)
 
-for i in movable_joints:
-    p.setJointMotorControl2(
-        bodyUniqueId=robot_id,
-        jointIndex=i,
-        controlMode=p.POSITION_CONTROL,
-        targetPosition=end_pos[k],
-        force=5
-    )
-    k=k+1
+
+movable_joints = [1, 2, 3, 4, 5]
 
 
 
+current_positions = []
+
+for joint in movable_joints:
+
+    joint_state = p.getJointState(robot_id, joint)
+
+    current_positions.append(joint_state[0])
+
+
+steps = 240
+
+for step in range(steps):
+
+    alpha = step / steps
+
+    for k, joint in enumerate(movable_joints):
+
+        target = current_positions[k] + alpha * (
+            joint_angles[k] - current_positions[k]
+        )
+
+        p.setJointMotorControl2(
+            bodyUniqueId=robot_id,
+            jointIndex=joint,
+            controlMode=p.POSITION_CONTROL,
+            targetPosition=target,
+            force=20
+        )
+
+    p.stepSimulation()
+
+    time.sleep(1 / 240)
 
 while True:
+
     p.stepSimulation()
+
     time.sleep(1 / 240)
